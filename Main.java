479487ab55004deef27216fab6117cda7be6ab59
diegocortes4 <diegocortes4@gmail.com>
2023-04-28 21:57:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        while(true){
+        Scanner sc = new Scanner(System.in);
+        // ask the user for their balance
+        // ask the user if they want esspresso or americano
+        // ask the user how many they want
+        // if the user inputs 0 or a negative number, produce no output
+        // if the user can not afford the number of coffees requested, produce no output
+        // the user should get a different message depending on the type of coffee they purchased
+        System.out.println("whats your balance?");
+        String balanceInput = sc.nextLine();
+        double balance = Double.parseDouble(balanceInput);
+        System.out.println("what type of coffee do you want? Enter espresso or latte.");
+        String coffeeInput = sc.nextLine();
+        System.out.println("how many do you want?");
+        int amountInput = Integer.parseInt(sc.nextLine());
+        double espressoPrice = 3.99;
+        double lattePrice = 2.99100;
+
+        if(amountInput > 0 ){
+            // proceed with coffee purchase attempt
+            boolean isEspresso = coffeeInput.equalsIgnoreCase("espresso");
+            boolean isLatte = coffeeInput.equalsIgnoreCase("latte");
+            if(isEspresso && (balance >= (espressoPrice * amountInput))){
+                // print out a successful espresso purchase message
+                System.out.println("enjoy your espresso");
+                
+            }
+            if(isLatte && (balance >= (lattePrice * amountInput))){
+                System.out.println("enjoy your latte");
+            }
+            
+        }else{
+            System.out.println("no coffee - amount requested is 0 or less");
+        }
+        // no coffee
+    }
+}
+}
